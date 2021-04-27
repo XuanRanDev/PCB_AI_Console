d835cdb705371aa8594507a7a20b4680982544e0
XuanRan0808 <3135419729@qq.com>
2021-04-27 12:00:30 +0800

    Initial commit

---
+++ src/sample/ApplicationInit.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.application.Application;
+
+/**
+ * Created By XuanRan on 2021/4/27
+ * 应用启动入口
+ */
+public class ApplicationInit {
+
+    public static void main(String[] args) {
+
+        Application.launch(Main.class);
+
+    }
+}
